2d64108a836b4d1ec1562acbfdf0c0111a6c59d8
Rob Moss <romoss@cisco.com>
2025-10-06 18:18:04 +1100

    Orgs, globals

--- CVA/provider.tf
+++ CVA/provider.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-      version = ">=1.0.64"
+      version = ">=1.0.61"      # Should be .61 for CVA
     }
     time = {
       source  = "time"
